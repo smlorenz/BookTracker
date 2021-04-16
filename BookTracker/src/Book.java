adcbd9998393f0411b9abda6f7bc6f746f2f2a22
smlorenz <smlorenz@knox.edu>
2021-04-15 19:17:44 -0500

    Making class

--- BookTracker/src/Book.java
+++ BookTracker/src/Book.java
@@ -1,4 +1,18 @@
 
 public class Book {
-	int pages;
+	private int pages;
+	private String author;
+	private String title;
+	private int rating;
+	
+	public Book(String title, String author, int pages, int rating) {
+		this.pages = pages;
+		this.author = author;
+		this.title = title;
+		this.rating = rating;
+	}
+	
+	public String toString() {
+		return String.format("Book %s by %s with %d pages has %d stars.", this.title, this.author, this.pages, this.rating);
+	}
 }
